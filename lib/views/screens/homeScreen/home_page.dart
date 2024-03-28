62d05f1d714b01a510b29ee3ed228ecdd1df57a9
rahul2210045 <rahul2210045@akgec.ac.in>
2024-03-28 13:22:36 +0530

    Screen Ui is added (LoginPage & SplashScreen)

---
+++ lib/views/screens/homeScreen/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(CupertinoIcons.home),
+        title: const Text("Programming Club"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+        ],
+      ),
+
+      //floating button to add new user
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_comment_outlined),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/splash_screen.dart
@@ -0,0 +1,59 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pc_app/main.dart';
+import 'package:pc_app/views/screens/landing_screen/landing_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 1500), () {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+      SystemChrome.setSystemUIOverlayStyle(
+          SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => const LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Welcome To Programming Club"),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+              top: mq.height * 0.15,
+              right: mq.width * 0.25,
+              width: mq.width * .5,
+              child: Image.asset("assets/icons/chat.png")),
+          Positioned(
+              bottom: mq.height * .15,
+              height: mq.height * 0.06,
+              left: mq.width * 0.05,
+              width: mq.width * .9,
+              child: const Text(
+                'MADE BY PC WITH ❤️',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 16, color: Colors.black87, letterSpacing: .5),
+              )),
+        ],
+      ),
+    );
+  }
+}
